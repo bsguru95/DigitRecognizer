287fdc0cf657707f3af4266f4844ca642d2d5f94
bsguru95 <bs.gururaj95@gmail.com>
2020-09-13 00:46:29 +0530

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+class Constants {
+
+  static double canvasSize = 300;
+  static double borderSize = 2;
+
+
+  static double imageSize = 300;
+  static int mnistImagesize = 28;
+
+  static double strokeWidth = 8;
+}
